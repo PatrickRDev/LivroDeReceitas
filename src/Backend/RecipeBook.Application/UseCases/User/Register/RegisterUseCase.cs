6c3309dadfb6dcb94a821586b45735a3b29f1275
PatrickDev <pkscriptrodrigues@gmail.com>
2025-08-20 19:01:19 -0300

    feat(user): adicionar RegisterUseCase

---
+++ src/Backend/RecipeBook.Application/UseCases/User/Register/RegisterUseCase.cs
@@ -0,0 +1,35 @@
+﻿using RecipeBook.Communication.Requests;
+using RecipeBook.Communication.Responses;
+
+namespace RecipeBook.Application.UseCases.User.Register
+{
+    public class RegisterUseCase
+    {
+
+        public ResponseRegisteredUserJson Execute(RequestRegisterUserJson request)
+        {
+
+            Validate(request);
+            return new ResponseRegisteredUserJson
+            {
+                Name = request.Name
+            };
+        }
+
+
+        private void Validate(RequestRegisterUserJson request)
+        {
+            var validator = new RegisterUserValidator();
+           var result = validator.Validate(request);
+
+            if(result.IsValid == false)
+            {
+                var errorMessages = result.Errors.Select(x => x.ErrorMessage).ToList();
+                throw new Exception();
+            }
+        }
+    }
+
+
+      
+}
---
+++ src/Backend/RecipeBook.Application/UseCases/User/Register/RegisterUserValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using RecipeBook.Communication.Requests;
+
+namespace RecipeBook.Application.UseCases.User.Register
+{
+    public class RegisterUserValidator : AbstractValidator<RequestRegisterUserJson>
+    {
+        public RegisterUserValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty();
+            RuleFor(x => x.Email).NotEmpty();
+            RuleFor(x => x.Email).EmailAddress();
+            RuleFor(x => x.Password.Length).GreaterThanOrEqualTo(6);
+
+        }
+    }
+}
